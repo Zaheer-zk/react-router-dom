2555dbe22e1ed6533704f9775e78e6ed7502d997
Zaheer-fexle <zaheer.khan@fexle.com>
2023-02-08 19:21:07 +0530

    Routing code added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
+import Error from "./Components/Error";
+import Header from "./Components/Header";
 import Home from "./Components/Home";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
 
 function App() {
   //React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
   return (
     <Router>
+      <Header/>
       <div className="App">
         <Routes>
           {/* Home */}
@@ -23,6 +25,9 @@ function App() {
 
           {/* Contact */}
           <Route path="/contact" element={<Contact />} />
+
+          {/* Error Page */}
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Components/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Error = () => {
+  return (
+    <div>
+      <Link>
+        <h1>Page not found 404</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
